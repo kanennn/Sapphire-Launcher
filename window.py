5105afe9da7d47f7d16c18e1dd0c138dc87e4efa
Pyrotex7 <83188404+Pyrotex7@users.noreply.github.com>
2021-11-24 14:57:30 -0600

    Remanaged .gitignore again.

--- window.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-import tkinter.messagebox as box
-
-testwindow = Tk()
-testwindow.title('Yes')
-button = Button(testwindow, text='Yes', command=exit)
-button.pack(padx=5,pady=2)
-testwindow.mainloop()
