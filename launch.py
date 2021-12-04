c0015f7432003b92bffbc046c3499d576c23b420
Pyrotex7 <83188404+Pyrotex7@users.noreply.github.com>
2021-12-04 12:52:29 -0600

    Added LWJGL, started on an authentication method, and downlaoded the requests module

--- launch.py
+++ launch.py
@@ -1,4 +1,15 @@
 import os
-
+import subprocess
+subprocess.Popen('Echo','Dude')
 def launch(version):
+    # Main launch handler
+    usertype = getusertype()
+    authtoken = getauthtoken(usertype)
+    input = assembleinput(version)
+
+def getusertype():
+    
+
+
+def assembleinput(version):
     pass
