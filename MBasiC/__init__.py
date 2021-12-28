11919fc1e1eb4c70338af2ea906ec5d040a15f0e
Pyrotex7 <83188404+Pyrotex7@users.noreply.github.com>
2021-12-27 21:24:47 -0600

    Added an attribute in __init__.py to give a path to use when calling relative folders whether in a frozen app or in script form.

--- MBasiC/__init__.py
+++ MBasiC/__init__.py
@@ -1,1 +1,6 @@
-# Empty initilization module to verify this folder as a package :D
+# (Mostly) Empty initilization module to verify this folder as a package :D
+import sys
+import os
+
+isFrozen = getattr(sys, 'frozen', False)
+path = getattr(sys, '_MEIPASS', os.getcwd())
